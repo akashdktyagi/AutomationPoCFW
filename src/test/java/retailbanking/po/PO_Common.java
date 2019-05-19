45b2cc509c04b26acc70fe1c6665435ee6875b04
akashdktyagi <akashdktyagi@gmail.com>
2019-05-19 21:24:35 +0530

    adding fw fils

---
+++ src/test/java/retailbanking/po/PO_Common.java
@@ -0,0 +1,34 @@
+package retailbanking.po;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.testng.Reporter;
+
+import core.utils.Utils;
+
+public class PO_Common {
+	
+	//Step 1 Driver
+	WebDriver driver;
+
+	//Step: Paramatrized constructor
+	public PO_Common(WebDriver d) {
+		driver = d;
+	}
+
+	//Elements or Locators
+	@FindBy(how = How.LINK_TEXT,using="Transfer Funds")
+	WebElement link_transfer_funds;
+	
+	public void ClickOnTransferFunds() {
+		try {
+			link_transfer_funds.click();
+			Utils.WriteLogs("info","Clicked on Transfer Funds");
+		}catch(Exception e) {
+			e.printStackTrace();
+			Utils.WriteLogs("fail","Unable to Click on Transfer Funds. Exception: " + e.toString());
+		}
+	}
+}
---
+++ src/test/java/retailbanking/tc/SmokeTest.java
@@ -0,0 +1,67 @@
+package retailbanking.tc;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import retailbanking.po.PO_Common;
+import retailbanking.po.PO_Login;
+import retailbanking.po.PO_TransferFunds;
+import core.utils.*;
+
+public class SmokeTest {
+	
+	@Parameters({"url","browser"})
+	@Test
+	public void t_01_check_login(String url,String browserType) {
+		try {
+			//String url = "http://zero.webappsecurity.com/login.html";
+			WebDriver driver = Utils.InvokeBrowser(browserType, url);
+
+			//Init Page Object
+			PO_Login PO_Login = PageFactory.initElements(driver, PO_Login.class);
+			PO_Login.KW_LoginInToApplication("username", "password");
+			
+			Utils.WriteLogs("pass","Test Case completed");
+		}catch(Exception e) {
+			Utils.WriteLogs("fail","Test failed");
+			Assert.assertFalse(false);
+		}
+
+	}
+	
+	@Parameters({"url","browser"})
+	@Test
+	public void t_02_check_transfer_funds(String url,String browserType)  {
+		WebDriver driver = Utils.InvokeBrowser(browserType, url);
+		//Init Page Object
+		PO_Login PO_Login = PageFactory.initElements(driver, PO_Login.class);
+		PO_Login.KW_LoginInToApplication("username", "password");
+		
+		//Click on Transfer Funds
+		PO_Common PO_Common = PageFactory.initElements(driver, PO_Common.class);
+		PO_Common.ClickOnTransferFunds();
+		
+		//Transfer Funds
+		PO_TransferFunds PO_TransferFunds = PageFactory.initElements(driver, PO_TransferFunds.class);
+		String from = "1";
+		String to = "2";
+		String ammount = "10";
+		String descp = "test";
+		
+		PO_TransferFunds.KW_FillTransferFundsPageAndSubmit(from, to, ammount, descp);
+		Utils.WriteLogs("pass","Test Case completed");
+		
+	}
+	
+	//<parameter name="url_parabank" value="http://parabank.parasoft.com/parabank/index.htm" />/
+	//@Parameters({"url_icici"})
+	//@Test(groups="smoke",description="Smoke Test For Search funtionality",enabled=true)
+}
